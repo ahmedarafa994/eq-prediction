37d8c564bfca3bfbc158d1061a10e9f568508110
ahmedarafa994 <ahmedarafa994@users.noreply.github.com>
2026-04-05 19:30:40 +0000

    Remove checkpoints and lightning_logs from repository

--- create_dummy.py
+++
@@ -1,7 +0,0 @@
-import torch
-import torchaudio
-
-# Create a 4 second 1-channel dummy audio file
-waveform = torch.randn(1, 22050 * 4)
-torchaudio.save("dummy.wav", waveform, 22050)
-print("Saved dummy.wav")
--- test_grad.py
+++
@@ -1,7 +0,0 @@
-import torch
-from torch.cuda.amp import autocast
-x = torch.zeros(2, requires_grad=True).cuda()
-with autocast():
-    l = torch.norm(x, p='fro')
-l.backward()
-print(x.grad)
